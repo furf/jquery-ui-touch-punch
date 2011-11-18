51655b9cac9f4cbe910c2d4995fe90a7a48e6089
Dave Furfero <furf@furf.com>
2011-11-18 18:08:34 -0500

    fix cascading issue (#16), trigger document mousedown on touchstart (#7, #16), added mouseover/mouseout support (#7), refactored event type translation (#7)

--- jquery.ui.touch-punch.js
+++ jquery.ui.touch-punch.js
@@ -43,6 +43,18 @@
     var self = this;
 
     self.element.bind('touchstart.' + self.widgetName, function (event) {
+
+      // fix an error where the mouse widget's mouseHandled never got reset
+      // because of a missing mousedown event on the root
+      // https://github.com/fracmak/jquery-ui-touch-punch/commit/622ef1365458c2327b1dd73ed480b26ba49e9d7d
+      // triggering mousedown on document
+      // https://github.com/hconceicao/jquery-ui-touch-punch/commit/d54be9253a432c37abcc007240145ef41aaf0b24
+      $doc.trigger('mousedown');
+
+      // triggering mouseover on the target
+      // https://github.com/hconceicao/jquery-ui-touch-punch/commit/d54be9253a432c37abcc007240145ef41aaf0b24
+      $(event.target).trigger(translateTouchEventToMouseEvent(event, 'mouseover'));
+
       return self._mouseDown(translateTouchEventToMouseEvent(event, 'mousedown'));
     });
 
@@ -51,6 +63,7 @@
     };
     
     self._touchEndDelegate = function(event) {
+      $(event.target).trigger(translateTouchEventToMouseEvent(event, 'mouseout'));
       return self._mouseUp(translateTouchEventToMouseEvent(event, 'mouseup'));
     };
 
@@ -59,8 +72,19 @@
 
   mouseProto._mouseDown = function (event) {
 
+    // fix an error where cascading touch handlers aren't handled correctly,
+    // this patch first checks if the event has already been handled before
+    // triggering the code
+    // https://github.com/fracmak/jquery-ui-touch-punch/commit/578ed6f1303a67e9205c9aca4b6f593cc8032fda
+    if (event.isDefaultPrevented()){
+      return;
+    }
+
     var self = this,
         ret  = _mouseDown.call(self, event);
+
+    // Prevent default in versions of jQuery UI < 1.8.6
+    event.preventDefault();
  
     $doc
       .bind('touchmove.' + self.widgetName, self._touchMoveDelegate)
