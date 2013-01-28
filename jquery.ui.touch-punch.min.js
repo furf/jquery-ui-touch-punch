72173a52e3b79c0e5ee8526c0b59fb75af24921b
Jon Keys <jon.keys@gmail.com>
2013-01-28 13:13:51 -0500

    Support for buggy webkit browsers (minified)

--- jquery.ui.touch-punch.min.js
+++ jquery.ui.touch-punch.min.js
@@ -1,4 +1,4 @@
-/*
+/*!
  * jQuery UI Touch Punch 0.2.2
  *
  * Copyright 2011, Dave Furfero
@@ -8,4 +8,12 @@
  *  jquery.ui.widget.js
  *  jquery.ui.mouse.js
  */
-(function(b){b.support.touch="ontouchend" in document;if(!b.support.touch){return;}var c=b.ui.mouse.prototype,e=c._mouseInit,a;function d(g,h){if(g.originalEvent.touches.length>1){return;}g.preventDefault();var i=g.originalEvent.changedTouches[0],f=document.createEvent("MouseEvents");f.initMouseEvent(h,true,true,window,1,i.screenX,i.screenY,i.clientX,i.clientY,false,false,false,false,0,null);g.target.dispatchEvent(f);}c._touchStart=function(g){var f=this;if(a||!f._mouseCapture(g.originalEvent.changedTouches[0])){return;}a=true;f._touchMoved=false;d(g,"mouseover");d(g,"mousemove");d(g,"mousedown");};c._touchMove=function(f){if(!a){return;}this._touchMoved=true;d(f,"mousemove");};c._touchEnd=function(f){if(!a){return;}d(f,"mouseup");d(f,"mouseout");if(!this._touchMoved){d(f,"click");}a=false;};c._mouseInit=function(){var f=this;f.element.bind("touchstart",b.proxy(f,"_touchStart")).bind("touchmove",b.proxy(f,"_touchMove")).bind("touchend",b.proxy(f,"_touchEnd"));e.call(f);};})(jQuery);
+(function(c){c.support.touch="ontouchend" in document;
+if(!c.support.touch){return;}var d=c.ui.mouse.prototype,f=d._mouseInit,b;function a(k){var i=window.pageXOffset,h=window.pageYOffset,g=k.clientX,j=k.clientY;
+if(k.pageY===0&&Math.floor(j)>Math.floor(k.pageY)||k.pageX===0&&Math.floor(g)>Math.floor(k.pageX)){g=g-i;j=j-h;}else{if(j<(k.pageY-h)||g<(k.pageX-i)){g=k.pageX-i;
+j=k.pageY-h;}}return{clientX:g,clientY:j};}function e(h,i){if(h.originalEvent.touches.length>1){return;}h.preventDefault();var k=h.originalEvent.changedTouches[0],g=document.createEvent("MouseEvents"),j=a(k);
+g.initMouseEvent(i,true,true,window,1,k.screenX,k.screenY,j.clientX,j.clientY,false,false,false,false,0,null);h.target.dispatchEvent(g);}d._touchStart=function(h){var g=this;
+if(b||!g._mouseCapture(h.originalEvent.changedTouches[0])){return;}b=true;g._touchMoved=false;e(h,"mouseover");e(h,"mousemove");e(h,"mousedown");};d._touchMove=function(g){if(!b){return;
+}this._touchMoved=true;e(g,"mousemove");};d._touchEnd=function(g){if(!b){return;}e(g,"mouseup");e(g,"mouseout");if(!this._touchMoved){e(g,"click");}b=false;
+};d._mouseInit=function(){var g=this;g.element.bind("touchstart",c.proxy(g,"_touchStart")).bind("touchmove",c.proxy(g,"_touchMove")).bind("touchend",c.proxy(g,"_touchEnd"));
+f.call(g);};})(jQuery);
