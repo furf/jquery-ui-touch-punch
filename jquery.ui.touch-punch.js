40769deeb1da547becec3289d90370ed615982d4
naitsirch <naitsirch@e.mail.de>
2022-07-25 12:37:39 +0200

    Defining jquery-ui as dependency instead of jquery.ui
    In the AMD definition the 'jquery.ui' dependency is invalid. Building a project with webpack results in this error:
    
    > ERROR  Failed to compile with 1 errors
    >
    > This dependency was not found:
    >
    > * jquery.ui in ./node_modules/@rwap/jquery-ui-touch-punch/jquery.ui.touch-punch.js
    
    Using 'jquery-ui' fixes that.

--- jquery.ui.touch-punch.js
+++ jquery.ui.touch-punch.js
@@ -19,7 +19,7 @@
     if ( typeof define === "function" && define.amd ) {
 
         // AMD. Register as an anonymous module.
-        define([ "jquery", "jquery.ui" ], factory );
+        define([ "jquery", "jquery-ui" ], factory );
     } else {
 
         // Browser globals
